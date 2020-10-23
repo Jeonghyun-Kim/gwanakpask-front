068fb4634416d35ac003809198ae5e84e70ead1e
Kay Kim <kimjh@bawi.org>
2020-10-23 23:43:25 +0900

    fix type bug

--- data/index.ts
+++ data/index.ts
@@ -4,7 +4,11 @@ import artistsJSON from './artists.json';
 const photos = photosJSON;
 const artists: Artist[] = artistsJSON;
 export const photosWithArtist: PhotoWithArtist[] = photos.map((photo) => {
-  const artist = artists.find((tmp) => tmp.artistId === photo.artistId);
+  const artist = artists.find((tmp) => tmp.artistId === photo.artistId) ?? {
+    artistId: -1,
+    position: '',
+    name: '',
+  };
   return {
     ...photo,
     artist,
